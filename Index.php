c529c4d1784b345910fcfafde319d619007de84e
santosh soti <soti93aansh@gmail.com>
2021-08-24 16:34:52 +0400

    first commit

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio Website</title>
+    <link rel="stylesheet" href="css/Style.css">
+    
+</head>
+<body>
+    <?php include 'Menu.php'; ?>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Bijaya Soti</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="#">About Me</a>
+            </div>
+        </div>
+    </section>
+
+        
+</body>
---
+++ Menu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BJ8X Blogger Website</title>
+    <link rel="stylesheet" href="css/Menu.css">
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Bijay<span>8X.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">Blogs</a></li>
+                <li><a href="#services" class="menu-btn">Store</a></li>
+                <li><a href="#skills" class="menu-btn">About Us</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
